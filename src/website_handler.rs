50f81cf89c085c08a3ac75df367524aa272b769e
LuCC-que <chen1968493689@gmail.com>
2022-08-09 09:54:46 -0400

    one more bug, the respond is not returned properly

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -4,6 +4,6 @@ pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()));
+        Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()))
     }
 }
