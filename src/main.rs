767ea92da63bc4408fe9862a9167bdf0d903d5c3
LuCC-que <chen1968493689@gmail.com>
2022-08-09 09:52:24 -0400

    bug fixed, the crate is not imported from main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![allow(dead_code)]
 use http::Method;
 use http::Request;
-
 use server::Server;
+use website_handler::WebsiteHandler;
 mod http;
 mod server; //order doesn't matter smart compiler
-
+mod website_handler;
 fn main() {
     let server = server::Server::new("127.0.0.1:8080".to_string());
-    server.run();
+    server.run(WebsiteHandler);
 }
